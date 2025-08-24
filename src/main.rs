9452e289d0377d0d6a69c54505624477cec66741
Michael Benedetti <michael.d.benedetti@gmail.com>
2025-08-24 16:58:04 -0400

    Wired up to pull freq block data

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-use sdr::FreqBlock;
+mod tui;
+
+use sdr::{FreqBlock, FreqSample};
 use tokio::select;
 use tokio::sync::mpsc::channel;
 use tokio::sync::watch::channel as watch_channel;
 use crate::tui::App;
-
-mod tui;
+use std::os::unix::net::UnixStream;
+use bincode::{Decode, Encode};
+use comms::{ConnectionType, External};
 
 #[tokio::main]
 async fn main() {
-    // Setup channels
-    // realtime freq blocks
     let (current_freq_block_tx, current_freq_block_rx) = watch_channel(FreqBlock::new());
-    // peaks
     let (peaks_tx, peaks_rx) = channel(512);
-    // center freq
     let (center_freq_tx, center_freq_rx) = channel(512);
 
-    // Start tui thread
     std::thread::spawn(move || {
         let terminal = ratatui::init();
         let app = App::new(
@@ -24,21 +22,20 @@ async fn main() {
             center_freq_rx,
             peaks_rx,
             2_000_000,
-            1_000_000,
+            100_000_000,
         );
 
         let _ = app.run(terminal);
         ratatui::restore();
     });
 
+    let config = bincode::config::standard().with_big_endian().with_fixed_int_encoding();
+    let mut stream = UnixStream::connect("/tmp/sdrscanner").unwrap();
+    bincode::encode_into_std_write(External::Connection(ConnectionType::Display), &mut stream, config).unwrap();
+
     loop {
-
+        if let Ok(External::Realtime(freq_block)) = bincode::decode_from_std_read(&mut stream, config) {
+            current_freq_block_tx.send(freq_block).unwrap();
+        }
     }
-
-    // // Handle data receipt from sdrscanner
-    // loop {
-    //     select!(
-    //         // Read from socket/tcp, depending on message type, dispatch to appropriate channel
-    //     )
-    // }
 }
